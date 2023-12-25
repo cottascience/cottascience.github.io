2f7451ca3aa9100dc05db6060871819f2a52adf4
Leonardo De Abreu Cotta <leonardodeabreucotta@gilda.ht.home>
2023-12-25 14:41:06 -0500

    bla

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-npx --yes -- single-page-markdown-website '*.md' --open
+git clone https://github.com/yuanqing/single-page-markdown-website
+npx --yes -- single-page-markdown-website '*.md'
 mv build/* ./
+rm -rf single-page-markdown-website
+open index.html
